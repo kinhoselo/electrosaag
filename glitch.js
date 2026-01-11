ff95edc9c0652cb14e84ddf92707af699f2eec44
kinhoselo <sfred3609@gmail.com>
2026-01-11 15:25:38 -0300

    Add files via upload

---
+++ glitch.js
@@ -0,0 +1,22 @@
+const glitch = document.getElementById("random-glitch");
+
+function randomGlitch() {
+  const vw = window.innerWidth;
+  const vh = window.innerHeight;
+
+  const size = Math.random() * 140 + 90;
+  glitch.style.width = size + "px";
+
+  glitch.style.left = Math.random() * (vw - size) + "px";
+  glitch.style.top = Math.random() * (vh - size) + "px";
+
+  glitch.style.opacity = 1;
+
+  setTimeout(() => {
+    glitch.style.opacity = 0;
+  }, Math.random() * 250 + 120);
+
+  setTimeout(randomGlitch, Math.random() * 5000 + 900);
+}
+
+randomGlitch();
